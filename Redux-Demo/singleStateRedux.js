a710d507f581411f757f1992040f0f5ba4cee58a
edward795 <ayyappadasgb777999@gmail.com>
2022-02-15 18:03:50 +0530

    1st commit

---
+++ Redux-Demo/singleStateRedux.js
@@ -0,0 +1,46 @@
+//redux- demo 
+
+//Actions 
+
+const BUY_CAKE='BUY_CAKE' 
+
+//Action Creators 
+
+function buyCake(){
+    return {
+        type:BUY_CAKE
+    }
+}
+
+//Reducers
+
+const initialState={
+    numOfCakes:10
+} 
+
+const reducer=(state=initialState,action)=>{
+    switch(action.type){
+        case BUY_CAKE:return{
+            ...state,
+            numOfCakes:state.numOfCakes-1
+        }
+
+        default:return state
+    }
+}
+
+//create a redux store
+const redux=require('redux')
+const createStore=redux.createStore 
+const store=createStore(reducer) 
+console.log('Initial State:',store.getState()) 
+
+//subscribe to it
+const unsubscribe=store.subscribe(()=>console.log('updated state:',store.getState())) 
+
+//dispatch actions
+store.dispatch(buyCake()) 
+store.dispatch(buyCake())
+store.dispatch(buyCake()) 
+
+unsubscribe()
